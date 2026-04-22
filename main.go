0e2b67f1c5eeaaf0af08b2af2ac3bc94eaad38b0
saku0512 <saku0512sec@gmail.com>
2026-04-22 10:52:20 +0900

    inti: project setup

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message:": "Gin server is running",
+		})
+	})
+
+	r.RUN(":8080")
+}
